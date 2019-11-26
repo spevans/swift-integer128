8f4d50f420697cd969785f3693df53cd70d56e46
Simon Evans <si@si.org>
2019-11-26 08:33:29 +0000

    Add more tests

--- Tests/Integer128Tests/UInt128Tests.swift
+++ Tests/Integer128Tests/UInt128Tests.swift
@@ -26,13 +26,14 @@ final class UInt128Tests: XCTestCase {
             ("1ffffffffffffffffffffffffffffffff",   16, nil,    nil),    // Overflow
             ("1",                                   10, 0,      1),
             ("11223344556677889900aabbccddeeff",    16, 0x1122334455667788, 0x9900aabbccddeeff),
-            ("abcdefghijklmnopqrstuvwxyz",          36, nil,    nil)
+            ("abcdefghijklmnopqrstuvwxyz",          36, nil,    nil),
+            ( "1" + String(repeating: "0", count: 63) + "1" + String(repeating: "0", count: 63), 2,  0x80000000_00000000, 0x80000000_00000000),
         ]
 
-        for test in testData {
-            let value = UInt128(test.0, radix: test.1)
-            XCTAssertEqual(value?._hiBits, test.2, "for input \"\(test.0)\"")
-            XCTAssertEqual(value?._loBits, test.3, "for input \"\(test.0)\"")
+        for data in testData {
+            let value = UInt128(data.0, radix: data.1)
+            XCTAssertEqual(value?._hiBits, data.2, "for input \"\(data.0)\"")
+            XCTAssertEqual(value?._loBits, data.3, "for input \"\(data.0)\"")
         }
     }
 
@@ -52,11 +53,11 @@ final class UInt128Tests: XCTestCase {
             (0xaaaaaaaa_aaaaaaaa,   0x55555555_55555555,    0,      0,      64),
         ]
 
-        for test in testData {
-            let number = UInt128(_hiBits: test.0, _loBits: test.1)
-            XCTAssertEqual(number.leadingZeroBitCount, test.2, "Leading Zero Bit Count of \(String(number, radix: 2)) != \(test.2)")
-            XCTAssertEqual(number.trailingZeroBitCount, test.3, "Trailing Zero Bit Count of \(String(number, radix: 2)) != \(test.3)")
-            XCTAssertEqual(number.nonzeroBitCount, test.4, "Non-Zero Bit Count of \(String(number, radix: 2)) != \(test.4)")
+        for data in testData {
+            let number = UInt128(_hiBits: data.0, _loBits: data.1)
+            XCTAssertEqual(number.leadingZeroBitCount, data.2, "Leading Zero Bit Count of \(String(number, radix: 2)) != \(data.2)")
+            XCTAssertEqual(number.trailingZeroBitCount, data.3, "Trailing Zero Bit Count of \(String(number, radix: 2)) != \(data.3)")
+            XCTAssertEqual(number.nonzeroBitCount, data.4, "Non-Zero Bit Count of \(String(number, radix: 2)) != \(data.4)")
         }
     }
 
@@ -71,10 +72,10 @@ final class UInt128Tests: XCTestCase {
             (0xffffffffffffffff,    0xffffffffffffffff, 0xffffffffffffffff, 0xffffffffffffffff),
         ]
 
-        for test in testData {
-            let number = UInt128(_hiBits: test.0, _loBits: test.1).byteSwapped
-            XCTAssertEqual(number._hiBits, test.2, "\(String(number._hiBits, radix:16)) != \(String(test.2, radix:16))")
-            XCTAssertEqual(number._loBits, test.3, "\(String(number._loBits, radix:16)) != \(String(test.3, radix:16))")
+        for data in testData {
+            let number = UInt128(_hiBits: data.0, _loBits: data.1).byteSwapped
+            XCTAssertEqual(number._hiBits, data.2, "\(String(number._hiBits, radix:16)) != \(String(data.2, radix:16))")
+            XCTAssertEqual(number._loBits, data.3, "\(String(number._loBits, radix:16)) != \(String(data.3, radix:16))")
         }
     }
 
@@ -87,23 +88,10 @@ final class UInt128Tests: XCTestCase {
         XCTAssertEqual(UInt128.zero, UInt128.min)
         XCTAssertGreaterThan(UInt128.max, UInt128.min)
         XCTAssertLessThan(UInt128.min, UInt128.max)
-
-
-        let (result1, borrow1) = UInt128.zero.subtractingReportingOverflow(UInt128(1))
-        XCTAssertTrue(borrow1)
-        XCTAssertEqual(result1._hiBits, 0xffff_ffff_ffff_ffff)
-        XCTAssertEqual(result1._loBits, 0xffff_ffff_ffff_ffff)
-
-        let (result2, borrow2) = UInt128.zero.subtractingReportingOverflow(result1)
-        XCTAssertTrue(borrow2)
-        XCTAssertEqual(result2._hiBits, 0)
-        XCTAssertEqual(result2._loBits, 1)
-
-        let (result3, borrow3) = UInt128.zero.subtractingReportingOverflow(UInt128(bit: 64))
-        XCTAssertTrue(borrow3)
-        XCTAssertEqual(result3._hiBits, 0xffff_ffff_ffff_ffff)
-        XCTAssertEqual(result3._loBits, 0)
-
+        XCTAssertEqual(UInt128.max.leadingZeroBitCount, 0)
+        XCTAssertEqual(UInt128.max.trailingZeroBitCount, 0)
+        XCTAssertEqual(UInt128.max.nonzeroBitCount, 128)
+        XCTAssertEqual(UInt128.bitWidth, 128)
 
         let ten = UInt128(10)
         XCTAssertEqual(ten._loBits, 10)
@@ -112,55 +100,47 @@ final class UInt128Tests: XCTestCase {
         XCTAssertEqual(shiftedTen._loBits, 0x4000_0000_0000_0000)
         XCTAssertEqual(shiftedTen._hiBits, 0x0000_0000_0000_0001)
 
-        let s1 = "63374607431768211455"
-        let uu1 = try XCTUnwrap(UInt128(s1))
-        XCTAssertEqual(uu1.description, s1)
-
-
         var u1 = UInt128()
         XCTAssertEqual(String(u1, radix: 2), "0")
         u1 += 100
         let u2 = u1 * 10
-        let u3 = try XCTUnwrap(UInt128("1000"))
+        let u3 = UInt128("1000")
         XCTAssertEqual(u2, u3)
 
 
         let maxNumberStr = "340282366920938463463374607431768211455"
         for length in 1..<maxNumberStr.count {
             let subStr = String(maxNumberStr.suffix(length))
-            let number = try XCTUnwrap(UInt128(subStr))
-            let str2 = number.description
+            let number = UInt128(subStr)
+            let str2 = number?.description
             if subStr.hasPrefix("0") {
                 XCTAssertEqual(str2, String(subStr.dropFirst()))
             } else {
                 XCTAssertEqual(str2, subStr)
             }
         }
-
-        let maxNumber = try XCTUnwrap(UInt128(maxNumberStr))
-        XCTAssertEqual(maxNumber.trailingZeroBitCount, 0)
-        XCTAssertEqual(maxNumber.leadingZeroBitCount, 0)
-        for word in maxNumber.words {
-            XCTAssertEqual(word, UInt.max)
-        }
-
-
-        let str = "10000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000"
-        let u4 = try XCTUnwrap(UInt128(str, radix: 2))
-        let u5 = UInt128(bit: 127)
-        XCTAssertEqual(u5._loBits, 0)
-        XCTAssertEqual(u5._hiBits, 0x8000_0000_0000_0000)
-        XCTAssertEqual(String(u5, radix: 2), str)
-        XCTAssertEqual(u4 >> 127, UInt128(1))
-        let u6 = ~u5
-        XCTAssertEqual(u6._loBits, 0xffff_ffff_ffff_ffff)
-        XCTAssertEqual(u6._hiBits, 0x7fff_ffff_ffff_ffff)
     }
 
     func testAddition() {
     }
 
     func testSubtraction() {
+
+
+        let testData: [(UInt64, UInt64, UInt64, UInt64, UInt64, UInt64, Bool)] = [
+            (0, 0, 0, 1, 0xffff_ffff_ffff_ffff, 0xffff_ffff_ffff_ffff, true),
+            (0, 0, 0xffff_ffff_ffff_ffff, 0xffff_ffff_ffff_ffff, 0, 1, true),
+            (0, 0, 0x1, 0, 0xffff_ffff_ffff_ffff, 0, true)
+        ]
+
+        for data in testData {
+            let number1 = UInt128(_hiBits: data.0, _loBits: data.1)
+            let number2 = UInt128(_hiBits: data.2, _loBits: data.3)
+            let (difference, borrow) = number1.subtractingReportingOverflow(number2)
+            XCTAssertEqual(difference._hiBits, data.4)
+            XCTAssertEqual(difference._loBits, data.5)
+            XCTAssertEqual(borrow, data.6)
+        }
     }
 
     func testMultiplication() {
@@ -195,18 +175,16 @@ final class UInt128Tests: XCTestCase {
             (UInt64.min,    UInt64.max),
         ]
 
-        for test in testData {
-            let number = UInt128(_hiBits: test.0, _loBits: test.1)
+        for data in testData {
+            let number = UInt128(_hiBits: data.0, _loBits: data.1)
             if UInt64.bitWidth == UInt.bitWidth {
-                print(String(number.words[0], radix: 16))
-                print(String(number.words[1], radix: 16))
-                XCTAssertEqual(number.words[0], test.1.words[0])
-                XCTAssertEqual(number.words[1], test.0.words[0])
+                XCTAssertEqual(number.words[0], data.1.words[0])
+                XCTAssertEqual(number.words[1], data.0.words[0])
             } else {
-                XCTAssertEqual(number.words[0], test.1.words[0])
-                XCTAssertEqual(number.words[1], test.0.words[1])
-                XCTAssertEqual(number.words[2], test.1.words[0])
-                XCTAssertEqual(number.words[3], test.0.words[1])
+                XCTAssertEqual(number.words[0], data.1.words[0])
+                XCTAssertEqual(number.words[1], data.0.words[1])
+                XCTAssertEqual(number.words[2], data.1.words[0])
+                XCTAssertEqual(number.words[3], data.0.words[1])
             }
         }
     }
