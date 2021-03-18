73cf74c9428844fa450a7775e617acff6bb24730
Simon Evans <si@si.org>
2021-03-18 11:18:22 +0000

    test: Make test.sh tests on Linux use swift 5.1, 5.2 and 5.3

--- test.sh
+++ test.sh
@@ -1,12 +1,21 @@
 #!/bin/sh
 
 MACOS_TESTS="Passed"
-LINUX_TESTS="Passed"
+LINUX_TESTS_LATEST="Passed"
+LINUX_TESTS_5_1="Passed"
+LINUX_TESTS_5_2="Passed"
+LINUX_TESTS_5_3="Passed"
 
 swift package clean
 swift test || MACOS_TESTS="Failed"
-docker build --tag=integer128-tests:$(date +%s) . || LINUX_TESTS="Failed"
+docker build --tag=integer128-tests:$(date +%s) -f Dockerfile . || LINUX_TESTS_LATEST="Failed"
+docker build --tag=integer128-tests:$(date +%s) -f Dockerfile-5.3 . || LINUX_TESTS_5_3="Failed"
+docker build --tag=integer128-tests:$(date +%s) -f Dockerfile-5.2 . || LINUX_TESTS_5_2="Failed"
+docker build --tag=integer128-tests:$(date +%s) -f Dockerfile-5.1 . || LINUX_TESTS_5_1="Failed"
 
 echo
 echo macOS tests ${MACOS_TESTS}
-echo Linux tests ${LINUX_TESTS}
+echo Linux latest tests ${LINUX_TESTS_LATEST}
+echo Linux 5.3 tests ${LINUX_TESTS_5_1}
+echo Linux 5.2 tests ${LINUX_TESTS_5_2}
+echo Linux 5.1 tests ${LINUX_TESTS_5_3}
